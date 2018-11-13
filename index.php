5e513764dd7090a8d54885c18611e432300eb655
Laurentiu Cerghedean <laurentiu.cerghedean@yahoo.com>
2018-11-13 20:52:37 +0200

    Update index.php
    Fragmented code in different files

--- index.php
+++ index.php
@@ -12,30 +12,8 @@ if(isset($_GET['id'])){
 }
 
 include('header.php');
-include('sidebar.php');
 
-?>
-
-<div id="content">
-      
-	  <?php
-
-		$sql_content	= 'select titlu_pagina,content from pages where id='.$id;
-		$result_content = $conn->query($sql_content);
-
-			if($result_content->num_rows){
-			
-				while($content = $result_content->fetch_assoc()){
-				
-					echo '<h1>'.$content['titlu_pagina'].'</h1>';
-					echo $content['content'];
-				}
-			}
-	  ?>
-        
-      </div>
-    </div>
-<?php
+include('page.php');
 
 include('footer.php');
 
